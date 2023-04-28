6e10444f44ee338f36a9ff471eb404a519ef183c
Andrei <73903075+solte-go@users.noreply.github.com>
2023-04-28 09:32:36 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -o pipefail
+
+setOutput() {
+    echo "${1}=${2}" >> "${GITHUB_OUTPUT}"
+}
+
+tagReg="^v?[0-9]+\.[0-9]+\.[0-9]+$"
+tag="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagReg" | head -n 1)"
+
+setOutput "tag" "$tag"
